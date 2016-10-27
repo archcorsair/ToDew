39bff2186628f9381741110442f7d925591012ed
Daniel Shneyder <archcorsair@gmail.com>
2016-10-27 16:40:04 -0700

    Fix spec failing lint

--- spec/.eslintrc.js
+++ spec/.eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: true,
       optionalDependencies: false,
+      // 'prefer-allow-callback': false,
+      // 'func-names': false,
+      // 'no-plusplus': false,
+      // 'space-before-function-paren': false,
+      // 'global-require': false,
     }],
   },
   env: {
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,38 +1,39 @@
 /* @flow */
 /* eslint-disable prefer-allow-callback */
 /* eslint-disable func-names */
+/* eslint-disable no-plusplus */
 /* eslint-disable space-before-function-paren */
+/* eslint-disable global-require */
 
 // This will allow us to have async callbacks to it and beforeEach
 import got from 'got';
 import loophole from 'loophole';
 import { it, beforeEach } from 'jasmine-fix';
 
-let getServerInstance
+let getServerInstance;
 // NOTE: express module uses eval() so we have to work it around in atom
 loophole.allowUnsafeEval(() => {
-  getServerInstance = require('../server/server')
-})
+  getServerInstance = require('../server/server');
+});
 
 describe('my awesome server', () => {
   let server;
   let port = 3000;
 
-  beforeEach(async function() {
+  beforeEach(async () => {
     port++;
-    server = await getServerInstance(port)
+    server = await getServerInstance(port);
   });
 
-  afterEach(function() {
-    server.close()
+  afterEach(() => {
+    server.close();
   });
 
-  it('works', async function() {
-    const response = await got(`http://localhost:${port}/`)
-    expect(response.statusCode).toBe(200)
+  it('works', async () => {
+    const response = await got(`http://localhost:${port}/`);
+    expect(response.statusCode).toBe(200);
 
     // NOTE: For API endpoints, compare full result instead of validating existance of chunks
-    expect(response.body).toContain('<div data-ng-view>')
+    expect(response.body).toContain('<div data-ng-view>');
   });
-
 });
